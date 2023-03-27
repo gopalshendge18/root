8f46bc03d438c77d3b3a99deaf2f4ad6e466a6d7
gopalshendge18 <85784714+gopalshendge18@users.noreply.github.com>
2023-03-27 15:05:58 +0530

    This files are just for demo purpose

---
+++ subsequence.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(vector<int>q,vector<int>&p,int index){
+
+if(index==p.size()){
+    for(auto it:q)
+    cout<<endl;
+    return ;
+}
+
+print(q,p,index+1);                                   
+                                                        
+
+q.push_back(p[index]);
+print(q,p,index+1);
+
+q.pop_back();
+
+
+
+}
+
+int main(){
+vector<int>p,q{1,2,3};
+ print(p,q,0);
+
+    return 0;
+}
